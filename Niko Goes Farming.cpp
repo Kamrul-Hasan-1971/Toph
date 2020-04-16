24f3338cdf336be4be04f00b4095034d149cc279
Kamrul_Hasan <48267671+Sara-Hasan@users.noreply.github.com>
2020-04-17 00:57:06 +0600

    Add files via upload

---
+++ Niko Goes Farming.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define  ll            long long int
+
+int main()
+{
+    ll  a,T;
+    cin>>T;
+    while(T--)
+    {
+        cin>>a;
+        ll x = a/2;
+        x=x/2;
+        cout<<x*x<<endl;
+    }
+    return 0 ;
+}
+//https://toph.co/p/niko-goes-farming
